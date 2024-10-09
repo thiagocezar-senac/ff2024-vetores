7b2efee21109a5c6f4dd8a8de2a8acfbc4f78140
thiago.tcezar@senacsp.edu.br <@tt875524>
2024-10-09 19:57:30 -0300

    feat: Vetores(Adicionando) e Exemplo 1.

---
+++ exemplo/exp1/script.js
@@ -0,0 +1,17 @@
+// vetor[0] = "Nome 1"; - Adiciona valor ao array
+// vetor.push("Nome 2"); - Adiciona valor na última posição
+// vetor.pop(); - Trás o último valor do array e exclui o mesmo
+
+// Declaro vetor
+let vetor = [];
+
+// Adiciono por posição
+vetor[0] = "Nome 1";
+
+// Adiciono na última posição
+vetor.push("Nome 2");
+
+
+// vetor.push(prompt("Insira um valor"));
+
+console.log(vetor);
