58b5fdec4690bf57f59202363bcc3cafbee3e9af
thiago.tcezar@senacsp.edu.br <@tt875524>
2024-10-09 21:47:30 -0300

    feat: Exemplo 3 - Incluindo (Includes).

--- exemplo/exp1/script3.js
+++ exemplo/exp1/script3.js
@@ -1,28 +1,34 @@
-// Crie um algoritmo que leia um vetor de 10 letras, e diga quantas consoantes foram lidas e mostre essas consoantes.
-
-let letras = [];
-let quantidade = 0; 
-let msg = "";  
-
-for (let i = 0; i < 10; i++) {
-    let letra = prompt(`Digite a ${i + 1} ª letra:`)
-    while (!isNaN(letra) || letra.trim() == '' || letra.length !== 1) {
-        alert("Letra Inválida!");
-        letra = prompt(`Digite a ${i + 1} ª letra novamente:`);
+/*
+Crie um algoritmo que leia um vetor de 10 letras, 
+e diga quantas consoantes foram lidas e mostre essas consoantes.
+*/
+function lerVetor() {
+    let letras = [];
+    for (let i = 0; i < 10; i++) {
+        let letra = prompt(`Digite a ${i + 1} ª letra:`);
+        while (!isNaN(letra) || letra.trim() == '' || letra.length !== 1) {
+            alert("Letra inválida!");
+            letra = prompt(`Digite a ${i + 1} ª letra novamente:`);
+        }
+        letras.push(letra);
     }
-    letras.push(letra);
+    return letras;
 }
 
-for (let j = 0; j < letras.length; j++) {
-    let letraLida = letras[j].toUpperCase();
-    let vgs = ["A","E","I","O","U"];
-    if(!(vgs[0] === letraLida) && !(vgs[1] === letraLida) &&
-       !(vgs[2] === letraLida) && !(vgs[3] === letraLida) &&
-       !(vgs[4] === letraLida)){
-
-        msg += letras[j] + " ";
-        quantidade++;
-
+function exibirConsoantes(letras) {
+    let msg = "";
+    let quantidade = 0;
+    for (let j = 0; j < letras.length; j++) {
+        let letraLida = letras[j].toUpperCase();
+        let vgs = ["A", "E", "I", "O", "U"];
+        if (!vgs.includes(letraLida)) {
+            msg += letras[j] + " ";
+            quantidade++;
+        }
     }
+    return `A quantidade de consoantes é ${quantidade} e as consoantes são elas: ${msg}`;
 }
-alert(`A quantidade de consoantes é ${quantidade} e as consoantes são elas: ${msg}`);
+
+let letrasVetor = lerVetor();
+let mensagem = exibirConsoantes(letrasVetor);
+alert(mensagem);
